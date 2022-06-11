a478905759aa1a6f0110f79334ae1998113b3327
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-11 18:36:45 +0300

    ex00, add if check

--- ex00/index.php
+++ ex00/index.php
@@ -1,13 +1,16 @@
 <?php
 	session_start();
-	$_SESSION['login'] = $_GET['login'];
-	$_SESSION['passwd'] = $_GET['passwd'];
+	if ($_GET['login'] && $_GET['passwd'] && $_GET['submit'] === "OK")
+	{
+		$_SESSION['login'] = $_GET['login'];
+		$_SESSION['passwd'] = $_GET['passwd'];
+	}
 ?>
 <html><body>
 <form method="GET" action="./index.php">
-	Username: <input type="text" required name="login" value="<?php echo ($_SESSION["login"]); ?>" />
+	Username: <input type="text" name="login" value="<?php echo ($_SESSION["login"]); ?>" />
 	<br />
-	Password : <input type="password" required name="passwd" value="<?php echo ($_SESSION["passwd"]); ?>" >
+	Password : <input type="password" name="passwd" value="<?php echo ($_SESSION["passwd"]); ?>" >
 	<input type="submit" name="submit" value="OK">
 </form>
 </body></html>
